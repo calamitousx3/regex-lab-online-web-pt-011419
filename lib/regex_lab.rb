d7277fa960ebddb8b1e2846e5ccc04f14c1e69e6
Carmen Renee <carmengenpt@gmail.com>
2019-02-06 17:22:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,10 @@
 require "pry"
 def starts_with_a_vowel?(word)
   
-  if word.match(/A[aeiou]/i) == nil 
-    return false 
-  else 
-    return true 
+ if word.match(/\A[aeiou]/i) == nil
+    return false
+  else
+    return true
   end
 end
 
