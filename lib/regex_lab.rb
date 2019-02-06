c181ad2d79da539475cf1b1737bb916521e34dea
Carmen Renee <carmengenpt@gmail.com>
2019-02-06 17:20:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
+require "pry"
 def starts_with_a_vowel?(word)
-
+  if word.match(/A[aeiou]/i) == nil 
+    return false 
+  else 
+    true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
